1767d9ee27194ce8411766315e8e448d2dba5a7e
Eric Wold <ericewold@gmail.com>
2020-12-06 16:23:43 -0700

    unused import removed

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,7 +1,6 @@
 import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
-import { idText } from 'typescript';
 
 @Resolver()
 export class PostResolver {
