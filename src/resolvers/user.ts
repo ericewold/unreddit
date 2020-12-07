148d525f9f05943486e92b3bac68a4b6758ce752
Eric Wold <ericewold@gmail.com>
2020-12-06 17:52:31 -0700

    username & password length validation. username must be unique now handled.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -38,19 +38,55 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'length must be greater than 2'
+          }
+        ]
+      };
+    }
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'length must be greater than 2'
+          }
+        ]
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword
     });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      if (err.detail.includes('already exists')) {
+        //duplicate username error
+        return {
+          errors: [
+            {
+              field: 'username',
+              message: 'that username already taken'
+            }
+          ]
+        };
+      }
+      console.log('message: ', err);
+    }
+    return { user };
   }
+
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
