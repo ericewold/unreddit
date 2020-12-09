ab6ff1dc417e483814e831a53455537b8f7a929a
Eric Wold <ericewold@gmail.com>
2020-12-09 16:19:00 -0700

    dev code snippet for purging user table

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  //await orm.em.nativeDelete(User, {}); // delete all users
   await orm.getMigrator().up();
 
   const app = express();
