0a2ce532d3a1ca1f9ba361c6b2047625f9eb042a
Eric Wold <ericewold@gmail.com>
2020-12-21 20:55:21 -0700

    use dataloader to optimize queries for users and votes

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import { createUpvoteLoader } from "./utils/createUpvoteLoader";
+import { createUserLoader } from "./utils/createUserLoader";
 
 export type MyContext = {
   req: Request & { session?: Express.Session }; //@1:54:58
   redis: Redis;
   res: Response;
+  userLoader: ReturnType<typeof createUserLoader>;
+  upvoteLoader: ReturnType<typeof createUpvoteLoader>;
 };
